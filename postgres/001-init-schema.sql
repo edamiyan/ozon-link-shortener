6f3d925081d68f8de229e5d489b6f3dd9d75e292
edamiyan <ed.amiyan@ya.ru>
2021-10-25 23:58:26 +0300

    Unify token field

--- postgres/001-init-schema.sql
+++ postgres/001-init-schema.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS links
 (
     id      serial        not null,
     baseURL varchar(1024) not null,
-    token   varchar(10)   not null
+    token   varchar(10)   not null,
+    UNIQUE (token)
 );
